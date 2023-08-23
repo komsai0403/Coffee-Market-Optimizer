c58e3481bc1ec0704de16e9c750a7b0cd8d1fbd2
alex <73740276+aahlexxx@users.noreply.github.com>
2023-08-23 10:05:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import numpy as np
+from itertools import product
 
 st.title("Coffee Market Optimizer")
 st.write("---")
@@ -30,23 +32,6 @@ if coffee_type == "Green Coffee Beans" or coffee_type == "Both":
     amount_gcb = st.number_input(label="Enter the amount of coffee cherries you have successfully converted to Green Coffee Beans (in kilos)", min_value=0.00, step=0.05)
 
 if coffee_variety == "Robusta":
-    lb_hauling = 1
-    lb_floating = 0.4167
-    lb_depulping = 2
-    lb_drying = 0.8685
-    lb_dehulling = 2
-    lb_sorting = 0.9925
-    lb_storage = 0
-
-    lb_pruning = 0.5
-    lb_fertilizing = 0.3125
-    lb_spraying = 0.3875
-    lb_weeding = 1
-    lb_harvesting = 0.8
-    lb_rejuvenation = 0.075
-
-    lb_transport = 0.4688
-
     m_hauling = 2.5
     m_floating = 0.4167
     m_depulping = 4.8
@@ -62,43 +47,9 @@ if coffee_variety == "Robusta":
     m_harvesting = 2
     m_rejuvenation = 0.225
 
-    m_transport = 1
-
-    ub_hauling = 5
-    ub_floating = 0.4167
-    ub_depulping = 10.9881
-    ub_drying = 7.7778
-    ub_dehulling = 5.2721
-    ub_sorting = 4.2273
-    ub_storage = 0
-
-    ub_pruning = 3
-    ub_fertilizing = 2.25
-    ub_spraying = 2
-    ub_weeding = 3.3333
-    ub_harvesting = 5
-    ub_rejuvenation = 1.1294
-
-    ub_transport = 2.1212 
+    m_transport = 1 
 
 elif coffee_variety == "Arabica":
-    lb_hauling = 0.625
-    lb_floating = 0
-    lb_depulping = 1.3636
-    lb_drying = 2.5133
-    lb_dehulling = 2
-    lb_sorting = 1.9329
-    lb_storage = 0
-
-    lb_pruning = 0.5
-    lb_fertilizing = 0.3125
-    lb_spraying = 0.3875
-    lb_weeding = 1
-    lb_harvesting = 0.8
-    lb_rejuvenation = 0.075
-
-    lb_transport = 1
-
     m_hauling = 1.5
     m_floating = 0
     m_depulping = 2.1667
@@ -116,40 +67,7 @@ elif coffee_variety == "Arabica":
 
     m_transport = 1.5
 
-    ub_hauling = 1.7787
-    ub_floating = 0
-    ub_depulping = 7.5
-    ub_drying = 7.7597
-    ub_dehulling = 4.7839
-    ub_sorting = 7.25
-    ub_storage = 0
-
-    ub_pruning = 3
-    ub_fertilizing = 1.4318
-    ub_spraying = 1.3125
-    ub_weeding = 3.3333
-    ub_harvesting = 6.8025
-    ub_rejuvenation = 1.5
-
-    ub_transport = 2.8833
-
 elif coffee_variety == "Excelsa":
-    lb_hauling = 1.1667
-    lb_floating = 0
-    lb_depulping = 2.1429
-    lb_drying = 2.5149
-    lb_dehulling = 2.475
-    lb_sorting = 2.1429
-    lb_storage = 0
-
-    lb_pruning = 0.75
-    lb_fertilizing = 0.9
-    lb_spraying = 0.6711
-    lb_weeding = 1.44
-    lb_harvesting = 2.6846
-    lb_rejuvenation = 0.45833
-
-    lb_transport = 1.1765
 
     m_hauling = 1.6
     m_floating = 0
@@ -166,24 +84,7 @@ elif coffee_variety == "Excelsa":
     m_harvesting = 7.05
     m_rejuvenation = 0.75
 
-    m_transport = 1.8182
-
-    ub_hauling = 2.2222
-    ub_floating = 0
-    ub_depulping = 17.8014
-    ub_drying = 8.1433
-    ub_dehulling = 6.0909
-    ub_sorting = 19
-    ub_storage = 0
-
-    ub_pruning = 2.9
-    ub_fertilizing = 8
-    ub_spraying = 3
-    ub_weeding = 9.5313
-    ub_harvesting = 12
-    ub_rejuvenation = 1.5
-
-    ub_transport = 3 
+    m_transport = 1.8182 
 
 st.header("Input Costs:")
 fertilizer1 = st.number_input(label="Enter the cost of Fertilizer 1 per kilogram: ", min_value=0.00, step=0.05)
@@ -242,67 +143,84 @@ def calculate():
         herbicide * herbicide_qty +
         pesticide * pesticide_qty
     )
-    lb_labor_cost = (
-        (lb_pruning * pruning + lb_fertilizing * fertilizing +
-        lb_spraying * spraying + lb_weeding * weeding +
-        lb_harvesting * harvesting + lb_rejuvenation * rejuvenation) * coffee_trees
-        )
     m_labor_cost = (
         (m_pruning * pruning + m_fertilizing * fertilizing +
         m_spraying * spraying + m_weeding * weeding +
         m_harvesting * harvesting + m_rejuvenation * rejuvenation) * coffee_trees
-        )
-    ub_labor_cost = (
-        (ub_pruning * pruning + ub_fertilizing * fertilizing +
-        ub_spraying * spraying + ub_weeding * weeding +
-        ub_harvesting * harvesting + ub_rejuvenation * rejuvenation) * coffee_trees
-        )
-    lb_postproduction_cost = (
-        (lb_hauling * hauling + lb_floating * floating +
-        lb_depulping * depulping + lb_drying * drying +
-        lb_dehulling * dehulling + lb_sorting * sorting +
-        lb_storage * storage) * amount_gcb
         )
     m_postproduction_cost = (
         (m_hauling * hauling + m_floating * floating +
         m_depulping * depulping + m_drying * drying +
         m_dehulling * dehulling + m_sorting * sorting +
         m_storage * storage ) * amount_gcb
-        )
-    ub_postproduction_cost = (
-        (ub_hauling * hauling + ub_floating * floating +
-        ub_depulping * depulping + ub_drying * drying +
-        ub_dehulling * dehulling + ub_sorting * sorting +
-        ub_storage * storage ) * amount_gcb
-        )
-    
-    total_lb_bound_cost = (
-        input_cost + lb_labor_cost + lb_postproduction_cost + other_cost + (lb_transport * transport * amount_fc)
-        )   
+        )  
     total_m_bound_cost = (
         input_cost + m_labor_cost + m_labor_cost + other_cost + (m_transport * transport * amount_fc)
         )
-    total_ub_bound_cost = (
-        input_cost + ub_labor_cost + ub_labor_cost + other_cost + (ub_transport * transport * amount_fc)
+    trader_fc_profit = (
+      (traders_allocation/100 * amount_fresh) * trader_fc_price
         )
+    other_markets_fc_profit = (
+      (other_markets_allocation/100 * amount_fresh) * other_markets_fc_price
+        )
+    total_profit = (
+      (trader_fc_profit + other_markets_fc_profit) - (total_cost)
+        )
+
+    return input_cost, m_labor_cost, m_postproduction_cost, total_m_bound_cost
+  
     
+  def calculate_profit(traders_allocation, other_markets_allocation):
+      total_input_cost = fertilizer1_cost*fertilizer1_kilo + fertilizer2_cost*fertilizer2_kilo + organic_cost*organic_kilo + herbicides_cost*herbicides_kilo + pesticide_cost*pesticide_kilo
+      total_labor_cost = (m_pruning * pruning_cost + m_fertilizing * fertilizing_cost + m_spraying * spraying_cost + m_weeding * weeding_cost + m_harvesting * harvesting_cost + m_rejuvenation * rejuvenation_cost) * number_of_trees
+      total_cost = total_input_cost + total_labor_cost + other_cost
 
-    return input_cost, lb_labor_cost, m_labor_cost, ub_labor_cost, lb_postproduction_cost , m_postproduction_cost, ub_postproduction_cost, total_lb_bound_cost, total_m_bound_cost, total_ub_bound_cost
+      # Calculate profits from selling fresh coffee cherries to local traders and other markets
+      trader_fc_profit = (traders_allocation * amount_fresh) * trader_fc_price
+      other_markets_fc_profit = (other_markets_allocation * amount_fresh) * other_markets_fc_price
+
+      # Calculate total profit
+      total_profit = (trader_fc_profit + other_markets_fc_profit) - (total_cost)
+
+      return total_profit
+    
+  def optimize_market_allocations():
+      variables = ['traders_allocation', 'other_markets_allocation']
+      values = np.linspace(0, 1, num=21)  # num=11 (0.1), num=101 (0.01), num=21 (0.05)
+
+      # define a generator function to generate possible combinations of market allocation with intervals as defined in the values variable
+      def market_allocation_generator():
+          # generates all possible combinations of the elements in the values variable with a length equal to the number of markets
+          for combo in product(values, repeat=len(variables)):
+            # assign values to the markets by pairing the markets to the combination of values generated
+            allocation = dict(zip(variables, combo))
+            # restriction to ensure market allocation is equal to 1
+              if sum(allocation.values()) == 1:
+                yield allocation
+
+        # create a list for the market allocations
+        market_allocations = list(market_allocation_generator())
+        num_allocations = len(market_allocations)
+        profits = np.zeros(num_allocations)
+
+        max_profit = 0  # Initialize maximum profit
+        max_profit_case = market_allocations[0]  # Initialize with the first allocation
+        max_profit_value = calculate_profit(**max_profit_case)
+
+        # Calculate profits for each market allocation
+        for i, case in enumerate(market_allocations):
+            profits[i] = calculate_profit(**case)
+            if profits[i] > max_profit_value:
+                max_profit_value = profits[i]
+                max_profit_case = case
+
+        # Print overall maximum profit
+        print("\nOverall Maximum Profit:")
+        print(f"Market Allocation: {max_profit_case}")
+        print(f"Maximum Profit: {max_profit_value}")
+
+    optimize_market_allocations()
 
 
-if st.button("Calculate"):
-    input_cost, lb_labor_cost, m_labor_cost, ub_labor_cost, lb_postproduction_cost , m_postproduction_cost, ub_postproduction_cost, total_lb_bound_cost, total_m_bound_cost, total_ub_bound_cost = calculate()
-    st.write("Input Cost: ", input_cost)
-    st.write("Lower Bound Labor Cost: ", lb_labor_cost)
-    st.write("Median Labor Cost:", m_labor_cost)
-    st.write("Upper Bound Labor Cost:", ub_labor_cost)
+if st.button("Optimize") = optimize_market_allocations()
 
-    if coffee_type == "Green Coffee Beans" or coffee_type == "Both":
-        st.write("Lower Bound Post Production Cost:", lb_postproduction_cost)
-        st.write("Median Post Production Cost:", m_postproduction_cost)
-        st.write("Upper Bound Post Production Cost:", ub_postproduction_cost)
-
-    st.write("Other Costs: ", other_cost)
-    st.write("Total Lower Bound Cost: ", total_lb_bound_cost)
-    st.write("Median Total Cost: ", total_m_bound_cost)
-    st.write("Upper Bound Total Cost: ", total_ub_bound_cost)
